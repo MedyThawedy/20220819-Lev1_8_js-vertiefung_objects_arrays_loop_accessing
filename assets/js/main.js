c34f2620cabb1efea798d9941a7665dc47253c7c
MD <dhmedy@googlemail.com>
2022-08-19 15:42:16 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+let studentData = [
+    {
+        name: "Alex",
+        age: 23,
+        coop: false,
+        address: {
+            street: "Don Valley Business Park",
+            city: "Toronto",
+            postalCode: "ONM3C3E5"
+        },
+        emails: ["alex69@gmail.com", "alex123@yahoo.com"]
+    },
+    {
+        name: "Sandra",
+        age: 22,
+        coop: true,
+        address: {
+            street: "34 Lawrence Ave",
+            city: "Toronto",
+            postalCode: "ONM3C0E5"
+        },
+        emails: ["sandra@gmail.com", "sandra@yahoo.com"]
+    }
+];
+
+/*
+Greife mithilfe von forEach() oder map() auf Eigenschaften dieses Objekts zu.
+Schreibe Funktion für das Objekt, die in der Console folgendes ausgeben:
+name
+coop
+city
+emails
+*/
+
+function showObj() {
+
+    studentData.map(
+        ({ name, age, coop, address, emails }) => {
+            console.log('name : ' + name, ' , age : ' + age, ', coop ' + coop + ', emails :' + emails[0] + ' : ' + emails[1]);
+        }
+    );
+
+}
